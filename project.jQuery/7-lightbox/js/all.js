12254b96cb201a6232141568a9552d3ea954ca1b
jack206418070 <44224865+jack206418070@users.noreply.github.com>
2019-01-29 23:02:33 +0800

    swiper and lightBox mix test

--- project.jQuery/7-lightbox/js/all.js
+++ project.jQuery/7-lightbox/js/all.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $('.button').click(function(event) {
-    $('.text').fadeToggle(1000);
+    $('.text').fadeToggle(500);
     // fadeIn 預設隱藏的東西給打開
     // fadeOut 預設開啟的東西給關閉
   });
   
   // lightbox 效果
   lightbox.option({
-        'resizeDuration': 2000,
+        'resizeDuration': 300,
         'wrapAround': true
       });
   $(window).load(function() {
---
+++ project.jQuery/swiper-top/js/all.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay:{
+            delay:5000,
+        },
+        effect: "slide",
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+
+    });
+});
+
+var swiperHigh = document.querySelector(".swiper-container");
+swiperHigh.style.Height = "500px!important";
+console.log(window.innerHeight);
